d115d0dc5a98e527b110517d50321b86932a7f88
Pablo Donaire <pablodonaire@gmail.com>
2014-11-30 20:13:10 +0100

    Encrypted dropbox

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = "kr6p55qoskv87hw"
-  config.dropbox_app_secret = "poqyl1mladuxik2"
-  config.dropbox_access_token = "3rypd8s7iwu1kpsm"
-  config.dropbox_access_token_secret = "52qdz0shqvole0d"
-  config.dropbox_user_id = "361448211"
-  config.dropbox_access_type = "dropbox"
+  config.dropbox_app_key = ENV['DROPBOX_APP_KEY']
+  config.dropbox_app_secret = ENV['DROPBOX_APP_SECRET']
+  config.dropbox_access_token = ENV['DROPBOX_ACCESS_TOKEN']
+  config.dropbox_access_token_secret = ENV['DROPBOX_ACCESS_TOKEN_SECRET']
+  config.dropbox_user_id = ENV['DROPBOX_USER_ID']
+  config.dropbox_access_type = ENV['DROPBOX_ACCESS_TYPE']
 end
