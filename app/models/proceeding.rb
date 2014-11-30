c803cea2f849f2cefc460794445d939958107159
Pablo Donaire <pablodonaire@gmail.com>
2014-11-30 19:56:24 +0100

    Encrypted dropbox

--- app/models/proceeding.rb
+++ app/models/proceeding.rb
@@ -1,26 +1,4 @@
 class Proceeding < ActiveRecord::Base
-  #has_attached_file :carta_despido,
-  #  :storage => :dropbox,
-  #  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  #has_attached_file :finiquito,
-  #  :storage => :dropbox,
-  #  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  #has_attached_file :cedula_citacion,
-  #  :storage => :dropbox,
-  #  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  #has_attached_file :demanda,
-  #  :storage => :dropbox,
-  #  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  #has_attached_file :sentencia,
-  #  :storage => :dropbox,
-  #  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  #
-  #do_not_validate_attachment_file_type :carta_despido
-  #do_not_validate_attachment_file_type :finiquito
-  #do_not_validate_attachment_file_type :cedula_citacion
-  #do_not_validate_attachment_file_type :demanda
-  #do_not_validate_attachment_file_type :sentencia
-  
   mount_uploader :carta_despido, CartaDespidoUploader
   mount_uploader :cedula_citacion, CedulaCitacionUploader
   mount_uploader :demanda, DemandaUploader
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create(:login => "jdonaire", :password => "161051", :password_confirmation => "161051")
